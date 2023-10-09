e2a26e7a42a685a33a2d0e24268570963b7bdcb3
TheKoren <korenzol.007@gmail.com>
2023-10-09 18:09:45 +0200

    Killed bt from example, data to std output

---
+++ simplicity/bt_soc_thunderboard_brd4166a/GNU ARM v10.3.1 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/36302/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32XG12/GCC/binapploader.o C:/Users/36302/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32XG12/GCC/libbluetooth.a C:/Users/36302/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32XG12/GCC/libpsstore.a C:/Users/36302/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg12_gcc_release.a
+
+LIBS := -lgcc -lc -lm -lnosys
+
