226d786680ac60747664dfcb643016f51b5db7b8
TheKoren <korenzol.007@gmail.com>
2023-11-13 19:30:44 +0100

    Added implementation for ESP8266 monitoring node

--- idf/station/main/myWifi.c
+++ idf/station/main/myWifi.c
@@ -33,15 +33,12 @@ void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, in
 
 void initialise_wifi(void)
 {
-	
- // 1 - Wi-Fi/LwIP Init Phase
-    esp_netif_init(); // TCP/IP initiation 					s1.1
-	esp_event_loop_create_default(); // event loop 			s1.2
-	esp_netif_create_default_wifi_sta(); // WiFi station 	s1.3
+    esp_netif_init();
+	esp_event_loop_create_default();
+	esp_netif_create_default_wifi_sta();
     wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&wifi_initiation); // 					s1.4
+    esp_wifi_init(&wifi_initiation);
 	
-	// 2 - Wi-Fi Configuration Phase
 	esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
     esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
     wifi_config_t wifi_configuration = {
@@ -50,9 +47,7 @@ void initialise_wifi(void)
             .password = CONFIG_STA_WIFI_PASSWORD}};
     esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
 
-	// 3 - Wi-Fi Start Phase
     esp_wifi_start();
 
-	// 4- Wi-Fi Connect Phase
 	esp_wifi_connect();
 }
